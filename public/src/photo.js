32dbc36117406d4f6d296b5c1122cb15079f1e53
skarimo <karimovj@gmail.com>
2018-10-30 23:36:59 -0400

    test

--- public/src/photo.js
+++ public/src/photo.js
@@ -2,7 +2,7 @@
 function loadCamera() {
   if(navigator.mediaDevices || navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } } }).then(function(stream) {
           video.src = window.URL.createObjectURL(stream);
           localstream = stream
           video.play();

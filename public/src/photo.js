7f8a56c4dd8466f4137cf301b9b4da0c3a2c8780
skarimo <karimovj@gmail.com>
2018-10-30 23:30:08 -0400

    test

--- public/src/photo.js
+++ public/src/photo.js
@@ -1,6 +1,6 @@
 
 function loadCamera() {
-  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  if(navigator.mediaDevices || navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
       navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
           video.src = window.URL.createObjectURL(stream);
